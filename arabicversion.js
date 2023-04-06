ab6b61d1490207c67a18f629009ea02638dd841d
Bhavanikodur0706 <117911802+Bhavanikodur0706@users.noreply.github.com>
2023-04-06 13:35:55 +0400

    Add files via upload

---
+++ arabicversion.js
@@ -0,0 +1,29 @@
+// Set default language to English
+var currentLang = 'en';
+
+// Check if there is a language stored in local storage
+var storedLang = localStorage.getItem('lang');
+if (storedLang) {
+    currentLang = storedLang;
+}
+
+// Translate the page based on the current language
+translatePage(currentLang);
+
+// Handle language button clicks
+$('.btn-tran').click(function () {
+    currentLang = currentLang === 'en' ? 'ar' : 'en'; // toggle the language
+    localStorage.setItem('lang', currentLang); // store the language in local storage
+    translatePage(currentLang); // translate the page
+});
+
+// Translate the page based on the current language
+function translatePage(lang) {
+    $.getJSON('arabic.json', function (data) {
+        $('[data-translate]').each(function () {
+            var key = $(this).attr('data-translate');
+            var translatedText = data[lang][key];
+            $(this).html(translatedText);
+        });
+    });
+}
